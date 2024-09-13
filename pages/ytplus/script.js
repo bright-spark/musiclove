79679e7c88f75a149b7ee076c4ceb1e894e229a8
iiinvent <5949436+bright-spark@users.noreply.github.com>
2024-09-13 05:22:41 +0200

    defer

--- pages/ytplus/script.js
+++ pages/ytplus/script.js
@@ -92,19 +92,13 @@ function trackEvent(eventType, eventData) {
     mixpanel.track(eventType, eventData);
 }
 
-// Set up service worker
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js').then(
-            registration => console.log('ServiceWorker registration successful with scope: ', registration.scope),
-            error => console.log('ServiceWorker registration failed: ', error)
-        );
-    });
-}
-
 // Initialize the page
 document.addEventListener('DOMContentLoaded', () => {
-    loadBio();
+    loadBio(); 
     setupNavButtons();
-    selectButton(0); // Default button selection
+    selectButton(0);
+    trackEvent('Page View', { 'Page': 'Home' });
+    console.log('Page initialized');
 });
+
+ 
