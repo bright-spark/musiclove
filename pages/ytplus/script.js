0b63f6a00ac344edc3df9ecd63314483558a1b9d
martin. <hex0h@outlook.com>
2024-09-16 18:01:34 +0200

    refactor

--- pages/ytplus/script.js
+++ pages/ytplus/script.js
@@ -1,104 +1,121 @@
 // JSON data
 const data = {
-    "bio": {
-        "name": "John Doe",
-        "title": "Playlist Curator",
-        "description": "YouTube Plus is a curated playlist of the best music videos on YouTube. Enjoy the latest hits and dance tracks from around the world.",
-        "image": "profile.png"
+    bio: {
+      name: "John Doe",
+      title: "Playlist Curator",
+      description:
+        "YouTube Plus is a curated playlist of the best music videos on YouTube. Enjoy the latest hits and dance tracks from around the world.",
+      image:
+        "https://store-images.s-microsoft.com/image/apps.32119.13849106443982885.cfa0ad6e-c759-430b-9f8d-e49be201e898.82443491-35dd-48c1-9b39-caa39119cd39"
     },
-    "playlists": [
-        {
-            "title": "Hits",
-            "description": "Top US Tracks on YouTube.",
-            "url": "https://www.youtube.com/embed/?4QIZE708gJ4&list=RDCLAK5uy_kmPRjHDECIcuVwnKsx2Ng7fyNgFKWNJFs&controls=0",
-            "auto": "https://www.youtube.com/embed/?4QIZE708gJ4&list=RDCLAK5uy_kmPRjHDECIcuVwnKsx2Ng7fyNgFKWNJFs&controls=0&autoplay=1"
-        },
-        {
-            "title": "Club",
-            "description": "Fresh Dance & Club Tracks on YouTube.",
-            "url": "https://www.youtube.com/embed/?evJ6gX1lp2o&list=RDCLAK5uy_nhf3h98yS3LCk_bVNQu6GjWG7ARvMaiFQ&controls=0",
-            "auto": "https://www.youtube.com/embed/?evJ6gX1lp2o&list=RDCLAK5uy_nhf3h98yS3LCk_bVNQu6GjWG7ARvMaiFQ&controls=0&autoplay=1"
-        },
-        {
-            "title": "Pop",
-            "description": "Pop Gold Tracks on YouTube.",
-            "url": "https://www.youtube.com/embed/?v=hLQl3WQQoQ0&list=RDCLAK5uy_nHSqCJjDrW9HBhCNdF6tWPdnOMngOv0wA&controls=0",
-            "auto": "https://www.youtube.com/embed/?v=hLQl3WQQoQ0&list=RDCLAK5uy_nHSqCJjDrW9HBhCNdF6tWPdnOMngOv0wA&controls=0&autoplay=1"
-        },
-        {
-            "title": "Ballads",
-            "description": "Soft Rock Ballads on YouTube.",
-            "url": "https://www.youtube.com/embed/?v=r3Pr1_v7hsw&list=RDCLAK5uy_nyKVppE-RpLkeCcwLct4rvN9e8AAsS_qw&controls=0",
-            "auto": "https://www.youtube.com/embed/?v=r3Pr1_v7hsw&list=RDCLAK5uy_nyKVppE-RpLkeCcwLct4rvN9e8AAsS_qw&controls=0&autoplay=1"
-        },
-        {
-            "title": "Dance",
-            "description": "Today's Dance Tracks on YouTube.",
-            "url": "https://www.youtube.com/embed/?ouEl3qTLc0M&list=RDCLAK5uy_kLWIr9gv1XLlPbaDS965-Db4TrBoUTxQ8&controls=0",
-            "auto": "https://www.youtube.com/embed/?ouEl3qTLc0M&list=RDCLAK5uy_kLWIr9gv1XLlPbaDS965-Db4TrBoUTxQ8&controls=0&autoplay=1"
-        },
+    playlists: [
+      {
+        title: "Hits",
+        description: "Top US Tracks on YouTube.",
+        url:
+          "https://www.youtube.com/embed/?4QIZE708gJ4&list=RDCLAK5uy_kmPRjHDECIcuVwnKsx2Ng7fyNgFKWNJFs&controls=0",
+        auto:
+          "https://www.youtube.com/embed/?4QIZE708gJ4&list=RDCLAK5uy_kmPRjHDECIcuVwnKsx2Ng7fyNgFKWNJFs&controls=0&autoplay=1"
+      },
+      {
+        title: "Club",
+        description: "Fresh Dance & Club Tracks on YouTube.",
+        url:
+          "https://www.youtube.com/embed/?evJ6gX1lp2o&list=RDCLAK5uy_nhf3h98yS3LCk_bVNQu6GjWG7ARvMaiFQ&controls=0",
+        auto:
+          "https://www.youtube.com/embed/?evJ6gX1lp2o&list=RDCLAK5uy_nhf3h98yS3LCk_bVNQu6GjWG7ARvMaiFQ&controls=0&autoplay=1"
+      },
+      {
+        title: "Pop",
+        description: "Pop Gold Tracks on YouTube.",
+        url:
+          "https://www.youtube.com/embed/?v=hLQl3WQQoQ0&list=RDCLAK5uy_nHSqCJjDrW9HBhCNdF6tWPdnOMngOv0wA&controls=0",
+        auto:
+          "https://www.youtube.com/embed/?v=hLQl3WQQoQ0&list=RDCLAK5uy_nHSqCJjDrW9HBhCNdF6tWPdnOMngOv0wA&controls=0&autoplay=1"
+      },
+      {
+        title: "Ballads",
+        description: "Soft Rock Ballads on YouTube.",
+        url:
+          "https://www.youtube.com/embed/?v=r3Pr1_v7hsw&list=RDCLAK5uy_nyKVppE-RpLkeCcwLct4rvN9e8AAsS_qw&controls=0",
+        auto:
+          "https://www.youtube.com/embed/?v=r3Pr1_v7hsw&list=RDCLAK5uy_nyKVppE-RpLkeCcwLct4rvN9e8AAsS_qw&controls=0&autoplay=1"
+      },
+      {
+        title: "Dance",
+        description: "Today's Dance Tracks on YouTube.",
+        url:
+          "https://www.youtube.com/embed/?ouEl3qTLc0M&list=RDCLAK5uy_kLWIr9gv1XLlPbaDS965-Db4TrBoUTxQ8&controls=0",
+        auto:
+          "https://www.youtube.com/embed/?ouEl3qTLc0M&list=RDCLAK5uy_kLWIr9gv1XLlPbaDS965-Db4TrBoUTxQ8&controls=0&autoplay=1"
+      }
     ]
-};
-
-// Function to check if the device is iOS or Android
-function isMobile() {
+  };
+  
+  // Function to check if the device is iOS or Android
+  function isMobile() {
     return /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-}
-
-// Function to load bio content (initial page load)
-function loadBio() {
-    document.getElementById('profile-image').src = data.bio.image;
-    loadPlaylist(0);  // Load the first playlist (non-autoplay for non-mobile, autoplay for mobile)
-}
-
-// Function to load playlist content and update YouTube player
-function loadPlaylist(index) {
+  }
+  
+  // Function to load bio content (initial page load)
+  function loadBio() {
+    document.getElementById("profile-image").src = data.bio.image;
+    loadPlaylist(0); // Load the first playlist (non-autoplay for non-mobile, autoplay for mobile)
+  }
+  
+  // Function to load playlist content and update YouTube player
+  function loadPlaylist(index) {
     const playlist = data.playlists[index];
-    const youtubePlayer = document.getElementById('youtube-player');
-    
+    const youtubePlayer = document.getElementById("youtube-player");
+  
     // Load non-autoplay version first
     youtubePlayer.src = playlist.url;
-
+  
     // For iOS and Android, after a short delay, switch to autoplay version
     if (isMobile()) {
-        setTimeout(() => {
-            youtubePlayer.src = playlist.auto;
-        }, 5);  // Delay allows non-autoplay to load briefly before switching to autoplay
+      setTimeout(() => {
+        youtubePlayer.src = playlist.auto;
+      }, 5); // Delay allows non-autoplay to load briefly before switching to autoplay
     }
-}
-
-// Set up navigation buttons
-function setupNavButtons() {
+  }
+  
+  // Set up navigation buttons
+  function setupNavButtons() {
     data.playlists.forEach((playlist, index) => {
-        const button = document.getElementById(`btn-${index}`);
-        button.textContent = playlist.title;
-        button.addEventListener('click', () => {
-            loadPlaylist(index);
-            selectButton(index);
-        });
+      const button = document.getElementById(`btn-${index}`);
+      button.textContent = playlist.title;
+      button.addEventListener("click", () => {
+        loadPlaylist(index);
+        selectButton(index);
+      });
     });
-}
-
-// Update button states
-function selectButton(selectedId) {
+  }
+  
+  // Update button states
+  function selectButton(selectedId) {
     document.querySelectorAll('button[id^="btn-"]').forEach((button, index) => {
-        button.classList.toggle('selected', index === selectedId);
-        button.classList.toggle('unselected', index !== selectedId);
+      button.classList.toggle("selected", index === selectedId);
+      button.classList.toggle("unselected", index !== selectedId);
     });
-}
-
-// Track various events
-function trackEvent(eventType, eventData) {
+  }
+  
+  // Track various events
+  function trackEvent(eventType, eventData) {
     mixpanel.track(eventType, eventData);
-}
-
-// Initialize the page
-document.addEventListener('DOMContentLoaded', () => {
-    loadBio(); 
+  }
+  
+  // Make page visible after load
+  window.addEventListener("load", function () {
+    // When the page is fully loaded, make the text visible and allow scrolling
+    document.documentElement.classList.add("loaded");
+  });
+  
+  // Initialize the page
+  document.addEventListener("DOMContentLoaded", () => {
+    loadBio();
     setupNavButtons();
     selectButton(0);
-    trackEvent('Page View', { 'Page': 'Home' });
-    console.log('Page initialized');
-});
-
- 
+    trackEvent("Page View", { Page: "Home" });
+    console.log("Page initialized");
+  });
+  
