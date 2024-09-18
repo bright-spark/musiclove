b7cca31f71a020301845c96fcfac70064b4ee9f1
iiinvent <5949436+bright-spark@users.noreply.github.com>
2024-09-18 12:48:18 +0200

    Update styling and layout in ytplus/index.html and ytplus/style.css

--- pages/ytplus/script.js
+++ pages/ytplus/script.js
@@ -107,7 +107,7 @@ function trackEvent(eventType, eventData) {
 // Make page visible after load
 window.addEventListener("load", function () {
   // When the page is fully loaded, make the text visible and allow scrolling
-  document.documentElement.classList.add("loaded");
+  //document.documentElement.classList.add("loaded");
 });
 
 // Initialize the page
@@ -115,7 +115,7 @@ document.addEventListener("DOMContentLoaded", () => {
   loadBio();
   setupNavButtons();
   selectButton(0);
-  //document.documentElement.classList.add("loaded");
+  document.documentElement.classList.add("loaded");
   trackEvent("Page View", { Page: "Home" });
   console.log("Page initialized");
 });
