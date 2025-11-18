9828986010912df212d5c648d3943d6c881a788d
iiinvent <inventinred@icloud.com>
2025-11-18 08:30:40 +0200

    Revert "Reapply "Reapply "Fix critical security and performance issues from code review"""
    This reverts commit 3a407e529253c36aa7452874289487f19956d05d.

--- service-worker.js
+++ service-worker.js
@@ -17,11 +17,10 @@ const APP_SHELL = [
   './css/app.css',
   './css/thorium.min.css',
   './css/classes.css',
-  './css/custom.css',
-  './css/framework7-bundle.min.css',
   './js/app.js',
   './js/routes.js',
-  './js/framework/framework7-bundle.min.js',
+  './node_modules/framework7/framework7-bundle.min.css',
+  './node_modules/framework7/framework7-bundle.min.js',
   './font-awesome/css/font-awesome.min.css'
 ];
 
